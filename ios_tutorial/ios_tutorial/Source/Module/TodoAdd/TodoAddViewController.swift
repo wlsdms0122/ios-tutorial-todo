681814f6941e4db0e40bb6e1a12d897b431e61fa
Jeong Jin Eun <wlsdms0122@gmail.com>
2020-05-27 00:14:16 +0900

    Step 2. ToDo 추가 화면 레이아웃 구성

--- ios_tutorial/ios_tutorial/Source/Module/TodoAdd/TodoAddViewController.swift
+++ ios_tutorial/ios_tutorial/Source/Module/TodoAdd/TodoAddViewController.swift
@@ -10,12 +10,24 @@ import UIKit
 
 class TodoAddViewController: UIViewController {
     // MARK: - Layout
+    @IBOutlet private weak var scrollView: UIScrollView!
+    @IBOutlet private weak var descriptionTextView: UITextView!
+    @IBOutlet private weak var addButton: UIButton!
     
     // MARK: - Property
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // A-1. Layout 스타일 지정
+        scrollView.contentInset.bottom = 86
+        
+        descriptionTextView.layer.borderWidth = 1
+        descriptionTextView.layer.borderColor = UIColor.black.cgColor
+        
+        addButton.layer.borderColor = addButton.backgroundColor?.cgColor
+        addButton.layer.cornerRadius = 8
     }
     
     // MARK: - Private
