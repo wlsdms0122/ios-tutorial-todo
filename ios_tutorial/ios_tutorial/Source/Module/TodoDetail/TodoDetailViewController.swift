c769efba90464c1d8c6498312a77244c031e969a
Jeong Jin Eun <wlsdms0122@gmail.com>
2020-05-28 22:09:05 +0900

    Step 2. ToDo 상세 화면 추가

---
+++ ios_tutorial/ios_tutorial/Source/Module/TodoDetail/TodoDetailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TodoDetailViewController.swift
+//  ios_tutorial
+//
+//  Created by JSilver on 2020/05/28.
+//  Copyright © 2020 JSilver. All rights reserved.
+//
+
+import UIKit
+
+class TodoDetailViewController: UIViewController {
+    // MARK: - Layout
+    
+    // MARK: - Property
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Private
+    
+    // MARK: - Action
+}
