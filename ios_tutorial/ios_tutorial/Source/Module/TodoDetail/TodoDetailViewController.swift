f398e2dc94a0d1887dcd36dd59531bf5c4fa0e47
Jeong Jin Eun <wlsdms0122@gmail.com>
2020-05-28 22:49:27 +0900

    Step 2. ToDo 상세 화면 레이아웃 구성

--- ios_tutorial/ios_tutorial/Source/Module/TodoDetail/TodoDetailViewController.swift
+++ ios_tutorial/ios_tutorial/Source/Module/TodoDetail/TodoDetailViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class TodoDetailViewController: UIViewController {
     // MARK: - Layout
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var priorityLabel: UILabel!
     
     // MARK: - Property
     
